65907daa905032ef0df93b099f1034d30d2d0824
Jimmy Wan <jimmy@novilabs.com>
2017-10-11 23:17:45 +0000

    Fix setup.py to work.

--- setup.py
+++ setup.py
@@ -12,12 +12,13 @@ with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
 def get_reqs(requirement_file):
-    requirements = list(parse_requirements(requirement_file, session=pip.download.PipSession()))
+    from pip.req import parse_requirements
+    requirements = list(parse_requirements(requirement_file, session='hack'))
     return [str(r.req) for r in requirements]
 
 
 install_requires = []
-tests_require = get_reqs('requirements_dev.txt')
+test_requires = get_reqs('requirements_dev.txt')
 # for running pytest as `python setup.py test`, see
 # http://doc.pytest.org/en/latest/goodpractices.html#integrating-with-setuptools-python-setup-py-test-pytest-runner
 setup_requires = ['pytest-runner']
@@ -31,7 +32,7 @@ setup(
     url='https://github.com/alphagov/gunicorn-logstash-formatter',
     packages=find_packages(include=['gunicorn_logstash_formatter']),
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=install_requires,
     license="MIT license",
     zip_safe=False,
     keywords='gunicorn_logstash_formatter',
@@ -46,6 +47,6 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
+    tests_require=test_requires,
+    setup_requires=setup_requires,
 )
